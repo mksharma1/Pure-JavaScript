1fcb809eccd751c9a2cbf9279214a3b40f5584be
Mohit Sharma <mohit.sharma@unthinkable.co>
2020-08-13 09:41:47 +0530

    Assignment Pagination Done

--- Assignment1/list.js
+++ Assignment1/list.js
@@ -1,58 +1,158 @@
-Pagination();
 
-function Pagination(numPage) {
+window.onload = function() {
+  pagination.init();
+  
+}
+pagination= new Pagination();
+pagination.init();
+
+
+function Pagination() {
   const objJson = [
-    { adName: "adName 1" },
-    { adName: "adName 2" },
-    { adName: "adName 3" },
-    { adName: "adName 4" },
-    { adName: "adName 5" },
-    { adName: "adName 6" },
-    { adName: "adName 7" },
-    { adName: "adName 8" },
-    { adName: "adName 9" },
-    { adName: "adName 10" },
-    { adName: "adName 11" },
-    { adName: "adName 12" },
-    { adName: "adName 13" },
-    { adName: "adName 14" },
-    { adName: "adName 15" },
-    { adName: "adName 16" },
-    { adName: "adName 17" },
-    { adName: "adName 18" },
-    { adName: "adName 19" },
-    { adName: "adName 20" },
-    { adName: "adName 21" },
-    { adName: "adName 22" },
-    { adName: "adName 23" },
-    { adName: "adName 24" },
-    { adName: "adName 25" },
-    { adName: "adName 26" },
-    { adName: "adName 27" },
-    { adName: "adName 28" },
-    { adName: "adName 29" },
-    { adName: "adName 30" },
-    { adName: "adName 31" },
-    { adName: "adName 32" },
-    { adName: "adName 33" },
-    { adName: "adName 34" },
-    { adName: "adName 35" },
-    { adName: "adName 36" },
+    { adName: "adName 1",
+    roll: "1",
+  emailid:"example@example.com"  },
+    { adName: "adName 2",
+    roll: "2",
+    emailid:"example@example.com" },
+    { adName: "adName 3",
+    roll: "3",
+    emailid:"example@example.com" },
+    { adName: "adName 4",
+    roll: "4",
+    emailid:"example@example.com" },
+    { adName: "adName 5",
+    roll: "5",
+    emailid:"example@example.com" },
+    { adName: "adName 6",
+    roll: "6",
+    emailid:"example@example.com" },
+    { adName: "adName 7",
+    roll: "7",
+    emailid:"example@example.com" },
+    { adName: "adName 8",
+    roll: "8",
+    emailid:"example@example.com" },
+    { adName: "adName 9",
+    roll: "9",
+    emailid:"example@example.com" },
+    { adName: "adName 10",
+    roll: "10",
+    emailid:"example@example.com" },
+    { adName: "adName 11",
+    roll: "11",
+    emailid:"example@example.com" },
+    { adName: "adName 12",
+    roll: "12",
+    emailid:"example@example.com" },
+    { adName: "adName 13",
+    roll: "13",
+    emailid:"example@example.com" },
+    { adName: "adName 14",
+    roll: "14",
+    emailid:"example@example.com" },
+    { adName: "adName 15",
+    roll: "15",
+    emailid:"example@example.com" },
+    { adName: "adName 16",
+    roll: "16",
+    emailid:"example@example.com" },
+    { adName: "adName 17",
+    roll: "17",
+    emailid:"example@example.com" },
+    { adName: "adName 18",
+    roll: "18",
+    emailid:"example@example.com" },
+    { adName: "adName 19",
+    roll: "19",
+    emailid:"example@example.com" },
+    { adName: "adName 20",
+    roll: "20",
+    emailid:"example@example.com" },
+    { adName: "adName 21",
+    roll: "21",
+    emailid:"example@example.com" },
+    { adName: "adName 22",
+    roll: "22",
+    emailid:"example@example.com" },
+    { adName: "adName 23",
+    roll: "23",
+    emailid:"example@example.com" },
+    { adName: "adName 24",
+    roll: "24",
+    emailid:"example@example.com" },
+    { adName: "adName 25",
+    roll: "25",
+    emailid:"example@example.com" },
+    { adName: "adName 26",
+    roll: "26",
+    emailid:"example@example.com" },
+    { adName: "adName 27",
+    roll: "27",
+    emailid:"example@example.com" },
+    { adName: "adName 28",
+    roll: "28",
+    emailid:"example@example.com" },
+    { adName: "adName 29",
+    roll: "29",
+    emailid:"example@example.com" },
+    { adName: "adName 30",
+    roll: "30",
+    emailid:"example@example.com" },
+    { adName: "adName 31",
+    roll: "31",
+    emailid:"example@example.com" },
+    { adName: "adName 32",
+    roll: "32",
+    emailid:"example@example.com" },
+    { adName: "adName 33",
+    roll: "33",
+    emailid:"example@example.com" },
+    { adName: "adName 34",
+    roll: "34",
+    emailid:"example@example.com" },
+    { adName: "adName 35",
+    roll: "37",
+    emailid:"example@example.com" },
+    { adName: "adName 37",
+    roll: "38",
+    emailid:"example@example.com" },
+    { adName: "adName 38",
+    roll: "39",
+    emailid:"example@example.com" },
+    { adName: "adName 39",
+    roll: "40",
+    emailid:"example@example.com" },
+    { adName: "adName 40",
+    roll: "41",
+    emailid:"example@example.com" },
+    { adName: "adName 41",
+    roll: "41",
+    emailid:"example@example.com" },
+    { adName: "adName 42",
+    roll: "42",
+    emailid:"example@example.com" },
+    { adName: "adName 43",
+    roll: "43",
+    emailid:"example@example.com" },
+
   ];
 
   const prevButton = document.getElementById("button_prev");
   const nextButton = document.getElementById("button_next");
   const firstButton = document.getElementById("button_first");
-  const lastButton = document.getElementById("button_last");
+  const lastButton = document.getElementById("button_last");`
   const setNumPage = document.getElementById("setNumPage");
   const clickPageNumber = document.querySelectorAll(".clickPageNumber"); 
   const buttonChange = document.getElementById("button_change");
 
-  var numPage = setNumPage.value;
   var records_per_page;
   setNumPage.addEventListener('click',function() {
-	passValue(this.value);
-	changePage(current_page,this.value);
+  passValue(this.value);
+  current_page=1;
+  changePage(1,this.value);
+  
+  
   });
   
   function passValue(x) {
@@ -62,14 +162,13 @@ function Pagination(numPage) {
 
   let current_page = 1;
 
-//   const records_per_page = x;
-
+ 
   this.init = function () {
-    changePage(1);
+    changePage(1,5);
     selectedPage();
     clickPage();
     addEventListeners();
-    buttonchange();
+    
   };
 
   let addEventListeners = function () {
@@ -77,12 +176,9 @@ function Pagination(numPage) {
     nextButton.addEventListener("click", nextPage);
     firstButton.addEventListener("click", firstPage);
 	lastButton.addEventListener("click", lastPage);
-	buttonChange.addEventListener("click",buttonchange);
 	
   };
-  let buttonchange = function(){
-	  changePage(current_page,x);
-  }
+  
 
   let selectedPage = function () {
     let page_number = document
@@ -118,13 +214,12 @@ console.log(records_per_page);
 
     listingTable.innerHTML = "";
 
-    for (
-      var i = (page - 1) * records_per_page;
+    for (var i = (page - 1) * records_per_page;
       i < page * records_per_page && i < objJson.length;
       i++
     ) {
       listingTable.innerHTML +=
-        "<div class='objectBlock'>" + objJson[i].adName + "</div>";
+        "<tr class='objectBlock'><td>"+ objJson[i].roll + "</td><td>" +objJson[i].adName+ "</td><td>"+objJson[i].emailid+" </td></tr>";
       let pageNumber = document.getElementById("page_number");
       pageNumber.innerHTML = "";
 
@@ -169,9 +264,4 @@ console.log(records_per_page);
     });
   };
 
-  let numPages = function () {
-    return Math.ceil(objJson.length / records_per_page);
-  };
-}
-let pagination = new Pagination();
-pagination.init();
+};
